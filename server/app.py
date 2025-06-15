1539caad7b8416b0fa92eda0042530d678678eba
kiago <roy.kiago@student.moringaschool.com>
2025-06-15 11:10:43 +0300

    App

--- server/app.py
+++ server/app.py
@@ -9,7 +9,8 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-     db.init_app(app)
+
+    db.init_app(app)
     migrate.init_app(app, db)
 
     from .controllers.restaurant_controller import restaurant_bp
@@ -20,4 +21,4 @@ def create_app():
     app.register_blueprint(pizza_bp)
     app.register_blueprint(restaurant_pizza_bp)
 
-    return app
+    return app    
