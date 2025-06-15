72023f2f4747b9ffcd7d56ebed97f9832cb3a047
kiago <roy.kiago@student.moringaschool.com>
2025-06-15 11:26:52 +0300

    seed

--- server/seed.py
+++ server/seed.py
@@ -23,3 +23,16 @@ p3 = Pizza(name="Veggie", ingredients="Dough, Tomato Sauce, Bell Peppers, Olives
 # Add to DB session
 db.session.add_all([r1, r2, r3, p1, p2, p3])
 db.session.commit()
+
+# Create RestaurantPizzas
+print("Linking pizzas to restaurants...")
+rp1 = RestaurantPizza(price=10, restaurant_id=r1.id, pizza_id=p1.id)
+rp2 = RestaurantPizza(price=12, restaurant_id=r1.id, pizza_id=p2.id)
+rp3 = RestaurantPizza(price=8, restaurant_id=r2.id, pizza_id=p1.id)
+rp4 = RestaurantPizza(price=15, restaurant_id=r2.id, pizza_id=p3.id)
+rp5 = RestaurantPizza(price=9, restaurant_id=r3.id, pizza_id=p2.id)
+
+db.session.add_all([rp1, rp2, rp3, rp4, rp5])
+db.session.commit()
+
+print("🌱 Seeding complete!")
