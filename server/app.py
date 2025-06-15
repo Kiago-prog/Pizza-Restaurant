9c071124392bc0f9c52f45b9d1c6c6dee96e9037
kiago <roy.kiago@student.moringaschool.com>
2025-06-15 11:09:27 +0300

    App

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
