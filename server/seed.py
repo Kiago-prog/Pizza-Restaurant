9737529bad793a2ba8616b495a37ae64baa1e69a
kiago <roy.kiago@student.moringaschool.com>
2025-06-15 11:26:16 +0300

    seed

--- server/seed.py
+++ server/seed.py
@@ -7,3 +7,9 @@ from models.restaurant_pizza import RestaurantPizza
 print("Clearing database...")
 db.drop_all()
 db.create_all()
+
+# Create Restaurants
+print("Seeding restaurants...")
+r1 = Restaurant(name="Mama's Kitchen", address="123 Main St")
+r2 = Restaurant(name="Kiki's Pizza", address="456 Side Ave")
+r3 = Restaurant(name="Tony's Pizza Palace", address="789 Slice Blvd")
